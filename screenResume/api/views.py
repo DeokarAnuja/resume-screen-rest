a04d4734a06893cbb4cb6e4a2d947b5dfd292465
Anuja <anujadeokar28@gmail.com>
2022-03-20 16:08:49 +0530

    Recruiter Detail API

--- screenResume/api/views.py
+++ screenResume/api/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Recruiter
 from .serializers import RecruiterSerializer
+
 # recruiter list
 class RecruiterList(generics.ListCreateAPIView):
     queryset=Recruiter.objects.all().order_by('id')
+    serializer_class=RecruiterSerializer
+
+# recruiter detail
+class RecruiterDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset=Recruiter.objects.all()
     serializer_class=RecruiterSerializer
